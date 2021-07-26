40087560ae192d9f65cd0540879274f054b4d972
david <debriceno80@gmail.com>
2021-07-26 08:39:01 -0700

    hola

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fiirstRobotProgram.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class firstRobotProgram extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "FrontLeft");
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+            motor.setPower(-0.5);
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstProgram.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+public class firstProgram {
+
+    public static void main(String args[]) {
+
+        int a = ;
+        int b = 1;
+
+
+        if (false) {
+            System.out.println("hello world");
+        } else if (a > b) {
+            System.out.println("The condition was false");
+
+
+        }
+    }
+
+
+}
